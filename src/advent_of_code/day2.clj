d6335002d1cb854ee6a97cbcbcf9b6a7cfe83e84
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-11 20:10:41 +0000

    Implement a bit more of the solution for day 2

--- src/advent_of_code/day2.clj
+++ src/advent_of_code/day2.clj
@@ -6,9 +6,14 @@
   (let [opcode (nth program opcode-index)
         op1 (nth program (nth program (+ opcode-index 1)))
         op2 (nth program (nth program (+ opcode-index 2)))
-        result-index (nth program (+ opcode-index 3))]
-    [opcode op1 op2 result-index]
-    ))
+        result-index (nth program (+ opcode-index 3))
+        result (cond
+                 (= opcode 1) (+ op1 op2))
+        parts (split-at (+ 1 result-index) program)]
+    (concat
+     (butlast (first parts))
+     [result]
+     (last parts))))
 
 (defn intcode-computer []
   (let [program-string "1,0,0,0,99"
