9ed2a54588acc1e287fe9a1e7029fbc0b7ebf14b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-12 08:03:07 +0000

    Introduce some more helper functions

--- src/advent_of_code/day2.clj
+++ src/advent_of_code/day2.clj
@@ -1,6 +1,9 @@
 (ns advent-of-code.day2
   (:require [clojure.string :as str]))
 
+(defn replace-at [n coll replacement]
+  (let [parts (split-at (inc n) coll)]
+    (concat (butlast (first parts)) [replacement] (last parts))))
 
 (defn execute [program opcode-index]
   (let [opcode (nth program opcode-index)]
@@ -8,19 +11,17 @@
       (let [op1 (nth program (nth program (+ opcode-index 1)))
             op2 (nth program (nth program (+ opcode-index 2)))
             result-index (nth program (+ opcode-index 3))
-            result (operation op1 op2)
-            parts (split-at (+ 1 result-index) program)]
-        (concat
-         (butlast (first parts))
-         [result]
-         (last parts))
-        )
-      )
+            result (operation op1 op2)]
+        (replace-at result-index program result)))
     (cond
       (= opcode 1) (execute (do-operation +) (+ opcode-index 4))
       (= opcode 2) (execute (do-operation *) (+ opcode-index 4))
       (= opcode 99) program)))
 
-(defn intcode-computer [program-string]
-  (let [program (map #(Integer/parseInt %) (str/split program-string #","))]
-    (execute program 0)))
+(defn intcode-computer [program] (execute program 0))
+
+(defn to-program [program-string]
+   (map #(Integer/parseInt %) (str/split program-string #",")))
+
+(let [input "1,0,0,0,99"]
+  (intcode-computer (to-program input)))
