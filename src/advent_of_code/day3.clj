16653eb194e291de7889cfb424ae3670ab19c432
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-12 19:33:47 +0000

    Reimplement to-points in terms of offsets from the start point

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -4,10 +4,12 @@
 (defn to-points [path]
   (let [start [0 0]
         direction (first path)
-        length (Integer/parseInt (subs path 1))]
+        length (Integer/parseInt (subs path 1))
+        [dx dy] [1 0]]
 
     (reduce
-     (fn [acc x] (conj acc [x (second start)]))
+     (fn [acc n] (conj acc [(+ (first start) (* n dx))
+                            (+ (second start) (* n dy))]))
      (hash-set)
      (range 1 (inc length)))))
 
