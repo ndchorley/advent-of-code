b2fb975682f30391c755ea8ba15fa14396bca3c7
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-13 09:24:30 +0000

    Build set of points from multiple path segments

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -2,19 +2,24 @@
   (:require [clojure.string :as str]))
 
 (defn to-points [path]
-  (let [start [0 0]
-        direction (first path)
-        length (Integer/parseInt (subs path 1))
-        [dx dy] (cond
-                  (= direction \R) [1 0]
-                  (= direction \L) [-1 0]
-                  (= direction \U) [0 1]
-                  (= direction \D) [0 -1])]
+  (defn build-points [path-segments points start]
+    (if (empty? path-segments) points
+        (let [current-segment (first path-segments)
+              direction (first current-segment)
+              length (Integer/parseInt (subs current-segment 1))
+              [dx dy] (cond
+                        (= direction \R) [1 0]
+                        (= direction \L) [-1 0]
+                        (= direction \U) [0 1]
+                        (= direction \D) [0 -1])
+              new-points (reduce
+                          (fn [acc n] (conj acc [(+ (first start) (* n dx))
+                                                 (+ (second start) (* n dy))]))
+                          points
+                          (range 1 (inc length)))
+              new-start [(+ (first start) (* length dx)) (+ (second start) (* length dy))]]
 
-    (reduce
-     (fn [acc n] (conj acc [(+ (first start) (* n dx))
-                            (+ (second start) (* n dy))]))
-     (hash-set)
-     (range 1 (inc length)))))
+          (build-points (rest path-segments) new-points new-start))))
+  (build-points (str/split path #",") (hash-set) [0 0]))
 
-(to-points "D6")
+(to-points "R8,U5")
