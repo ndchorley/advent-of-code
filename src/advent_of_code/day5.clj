a0911c87776d9eb153ea3aac3df91c9a5e869085
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-19 15:12:18 +0000

    Day 5: Extract a to-operation function

--- src/advent_of_code/day5.clj
+++ src/advent_of_code/day5.clj
@@ -7,17 +7,18 @@
       (let [digit (mod rest 10)]
         (recur (/ (- rest digit) 10) (conj digits digit))))))
 
+(defn to-operation [opcode]
+  (cond
+    (= opcode 1) +
+    (= opcode 2) *
+    ; Might need something else here!
+    (= opcode 99) identity))
+
 (defn parse-operation [value]
   (let [digits (extract-digits value)
         [opcode-digits parameter-mode-digits] (split-at 2 digits)
         opcode (+ (first opcode-digits) (* (second opcode-digits) 10))]
-    {:operation
-     (cond
-       (= opcode 1) +
-       (= opcode 2) *
-       ; Might need something else here!
-       (= opcode 99) identity)
-     :parameter-modes parameter-mode-digits
-     }))
+    {:operation (to-operation opcode)
+     :parameter-modes parameter-mode-digits}))
 
 (map parse-operation [1001 1102 1099])
