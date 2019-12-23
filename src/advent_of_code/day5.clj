aa3fcf2370df6e99712472d87a7885e4987ea8b4
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-12-23 15:03:58 +0000

    Day 5: Support opcode 3

--- src/advent_of_code/day5.clj
+++ src/advent_of_code/day5.clj
@@ -4,7 +4,7 @@
 (defn extract-digits [value]
   (loop [rest value digits []]
     (if (zero? rest)
-      digits
+      (conj digits 0)
       (let [digit (mod rest 10)]
         (recur (/ (- rest digit) 10) (conj digits digit))))))
 
@@ -15,7 +15,7 @@
 
 (defn to-position-mode [bit] (if (= 0 bit) :position :immediate))
 
-(defn parse-operation [value]
+(defn parse-operation [value inputs]
   (let [digits (extract-digits value)
         [opcode-digits parameter-mode-bits] (split-at 2 digits)
         opcode (+ (first opcode-digits) (* (second opcode-digits) 10))]
@@ -24,6 +24,7 @@
                     :parameter-modes (map to-position-mode (default-missing-mode-bits parameter-mode-bits 3))}
       (= opcode 2) {:operation *
                     :parameter-modes (map to-position-mode (default-missing-mode-bits parameter-mode-bits 3))}
+      (= opcode 3) {:operation (fn [] (first inputs)) :parameter-modes [:position]}
       (= opcode 99) :stop)))
 
 (defn parameter-values [program operation-index modes]
@@ -34,9 +35,9 @@
        (nth program (+ operation-index i 1))))
    modes))
 
-(defn intcode-computer [program]
+(defn intcode-computer [inputs program]
   (loop [current-program program operation-index 0]
-    (let [operation (parse-operation (nth current-program operation-index))]
+    (let [operation (parse-operation (nth current-program operation-index) inputs)]
       (if (= operation :stop) current-program
           (let [parameters (parameter-values current-program operation-index (butlast (:parameter-modes operation)))
                 result-index (nth current-program (+ operation-index (count (:parameter-modes operation))))
@@ -45,4 +46,8 @@
              (day2/replace-at result-index current-program result)
              (+ operation-index (count (:parameter-modes operation)) 1)))))))
 
-(map (comp intcode-computer day2/to-program) ["1002,4,3,4,33" "1101,100,-1,4,0"])
+(map
+ (comp (partial intcode-computer []) day2/to-program)
+ ["1002,4,3,4,33" "1101,100,-1,4,0"])
+
+(intcode-computer [21] (day2/to-program "3,0,99"))
