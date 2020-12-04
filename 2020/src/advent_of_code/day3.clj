45bade0a9e54087ebfac0cb8e101647b3f4255c1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-12-04 07:17:36 +0000

    Add solution for day 3, part 2

--- 2020/src/advent_of_code/day3.clj
+++ 2020/src/advent_of_code/day3.clj
@@ -3,14 +3,14 @@
 
 (def input-map (read-lines "day3"))
 
-(defn points-visited [input-map]
+(defn points-visited [input-map [dx dy]]
   (let [width (count (first input-map))
         height (count input-map)]
 
     (take-while
      (fn [[x y]] (< y height))
      (map
-      (fn [n] [(mod (* 3 n) width) n])
+      (fn [n] [(mod (* n dx) width) (* n dy)])
       (range)))))
 
 (defn character-at [input-map [x y]]
@@ -18,9 +18,20 @@
 
 (defn tree? [character] (= character \#))
 
-(count
- (filter
-  tree? 
-  (map
-   (partial character-at input-map)
-   (points-visited input-map))))
+(defn count-trees [input-map slope]
+  (count
+   (filter
+    tree? 
+    (map
+     (partial character-at input-map)
+     (points-visited input-map slope)))))
+
+(count-trees input-map [3 1])
+
+(def slopes [[1 1] [3 1] [5 1] [7 1] [1 2]])
+
+(apply
+ *
+ (map
+  (partial count-trees input-map)
+  slopes))
