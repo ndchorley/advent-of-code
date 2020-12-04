afb395d5d2fe9d5a6218adaf9acf10de17fc556a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-12-04 09:20:50 +0000

    Add cider-nrepl

--- 2020/project.clj
+++ 2020/project.clj
@@ -1,4 +1,5 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[cider/cider-nrepl "0.25.5"]])
