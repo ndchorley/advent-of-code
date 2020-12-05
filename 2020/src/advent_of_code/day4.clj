25f60f0bf58ada2146eb97c5481c596407958edf
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-12-05 17:01:52 +0000

    Refactor day 4, part 1

--- 2020/src/advent_of_code/day4.clj
+++ 2020/src/advent_of_code/day4.clj
@@ -14,7 +14,8 @@
          result)))))
 
 (defn passport-boundaries [batch-file]
-  (let [blank-line-indexes (indexes-of "" batch-file)]
+  (let [blank-line-indexes
+        (indexes-of "" batch-file)]
     (partition
      2
      (concat
@@ -24,35 +25,54 @@
        (map inc blank-line-indexes))
       [(count batch-file)]))))
 
+(defn parse-line [line]
+  (let [matcher
+        (re-matcher
+         #"((?<field>[a-z]+):(?<value>[^\s]+))"
+         line)]
+    (into
+     {}
+     (map
+      #(subvec % (- (count %) 2) (count %))
+      (take-while
+       #(not (nil? %))
+       (repeatedly #(re-find matcher)))))))
+
+(defn parse-passport [lines]
+  (apply
+   merge
+   (map parse-line lines)))
+
+(defn group-passport-lines
+  [batch-file boundaries]
+  (map
+   (fn [[start end]]
+     (subvec batch-file start end))
+   boundaries))
+
 (defn passports [batch-file]
   (map
-   (fn [[start end]] (subvec batch-file start end))
-   (passport-boundaries batch-file)))
+   parse-passport
+   (group-passport-lines
+    batch-file
+    (passport-boundaries batch-file))))
 
-(defn extract-fields-in-line [line]
-  (let [matcher
-        (re-matcher #"((?<field>[a-z]+):[^\s]+)" line)]
-    (map
-     last
-     (take-while
-      #(not (nil? %))
-      (repeatedly #(re-find matcher))))))
-
-(defn fields [passport-lines]
-  (into
-   #{}
-   (mapcat extract-fields-in-line passport-lines)))
-
-(defn valid? [fields]
+(defn valid? [passport]
   (let [expected-fields
-        #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"}]
+        #{"byr" "iyr" "eyr" "hgt"
+          "hcl" "ecl" "pid"}]
     (=
      expected-fields
-     (clojure.set/intersection fields expected-fields))))
+     (clojure.set/intersection
+      (into #{} (keys passport))
+      expected-fields))))
 
-(count 
- (filter
-  valid?
-  (map
-   fields
-   (passports (read-lines "day4")))))
+(defn count-valid-passports [valid? passports]
+  (count
+   (filter
+    valid?
+    passports)))
+
+(count-valid-passports
+ valid?
+ (passports (read-lines "day4")))
