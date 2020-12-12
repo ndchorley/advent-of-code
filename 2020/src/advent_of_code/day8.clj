ac92bfcf50f6362ab864772f89988e6e9d7d85c1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-12-12 17:50:20 +0000

    Add solution for day 8, part 1

---
+++ 2020/src/advent_of_code/day8.clj
@@ -0,0 +1,37 @@
+(ns advent-of-code.day8
+  (:require
+   [advent-of-code.core :refer :all]
+   [clojure.string :as str]))
+
+(defn parse-line [line]
+  (let [[operation argument-string]
+        (str/split line #" ")]
+    [operation
+     (Integer/parseInt argument-string)]))
+
+(defn execute [instructions]
+  (loop [offset 0 accumulator 0 offsets-seen #{}]
+    (let [instruction (instructions offset)]
+      (if (contains? offsets-seen offset)
+        accumulator
+        (let [[operation argument] instruction
+              [next-offset new-accumulator]
+              (cond
+                (= operation "nop") [(inc offset)
+                                     accumulator]
+                (= operation "acc") [(inc offset)
+                                     (+
+                                      accumulator
+                                      argument)]
+                (= operation "jmp") [(+ offset argument)
+                                     accumulator]
+                )]
+          (recur
+           next-offset
+           new-accumulator
+           (conj offsets-seen offset)))))))
+
+(execute
+ (mapv
+  parse-line
+  (read-lines "day8")))
