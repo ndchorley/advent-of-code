b679dc16902197f085fb89bfdc56c15f73983040
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-11-28 07:11:39 +0000

    Add solution for 2021, day 2, part 1

---
+++ 2021/day2.clj
@@ -0,0 +1,48 @@
+(defn parse-line [line]
+  (let [[command amount_string] (str/split line #" ")]
+    {:command command
+     :amount (Integer/parseInt amount_string)}))
+
+(defn move-forwards [current-coordinates amount]
+  (let [new-horizontal-position
+        (+ (current-coordinates :horizontal-position) amount)]
+    (assoc
+     current-coordinates
+     :horizontal-position
+     new-horizontal-position)))
+
+(defn move-up [current-coordinates amount]
+  (let [new-depth
+        (- (current-coordinates :depth) amount)]
+    (assoc
+     current-coordinates
+     :depth
+     new-depth)))
+
+(defn move-down [current-coordinates amount]
+  (let [new-depth
+        (+ (current-coordinates :depth) amount)]
+    (assoc
+     current-coordinates
+     :depth
+     new-depth)))
+
+(let [commands
+      {"forward" move-forwards
+       "up" move-up
+       "down" move-down}
+
+      parsed-lines
+      (map parse-line (read-lines "day_2_input"))
+
+      final-coordinates
+      (reduce
+       (fn [current-coordinates next]
+         (let [command (commands (next :command))]
+           (command current-coordinates (next :amount))))
+       {:horizontal-position 0 :depth 0}
+       parsed-lines)]
+
+  (*
+   (final-coordinates :horizontal-position)
+   (final-coordinates :depth)))
