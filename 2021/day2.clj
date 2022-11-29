1a93143a02a00d4ffab19e7d6ff78f22ec08bd53
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-11-29 21:06:12 +0000

    Add solution for 2021, day 2, part 2

--- 2021/day2.clj
+++ 2021/day2.clj
@@ -5,23 +5,35 @@
 
 (defn move-forwards [current-coordinates amount]
   (let [new-horizontal-position
-        (+ (current-coordinates :horizontal-position) amount)]
+        (+
+         (current-coordinates :horizontal-position)
+         amount)
+
+        new-depth
+        (+
+         (current-coordinates :depth)
+         (* (current-coordinates :aim) amount))]
+
     (assoc
      current-coordinates
-     :horizontal-position
-     new-horizontal-position)))
 
-(defn change-depth [current-coordinates delta]
-  (let [new-depth (+ (current-coordinates :depth) delta)]
-    (assoc current-coordinates :depth new-depth)))
+     :horizontal-position
+     new-horizontal-position
+
+     :depth
+     new-depth)))
+
+(defn change-aim [current-coordinates delta]
+  (let [new-aim (+ (current-coordinates :aim) delta)]
+    (assoc current-coordinates :aim new-aim)))
 
 (defn move-up [current-coordinates amount]
   (let [delta (* -1 amount)]
-    (change-depth current-coordinates delta)))
+    (change-aim current-coordinates delta)))
 
 (defn move-down [current-coordinates amount]
   (let [delta amount]
-    (change-depth current-coordinates delta)))
+    (change-aim current-coordinates delta)))
 
 (let [commands
       {"forward" move-forwards
@@ -34,9 +46,11 @@
       final-coordinates
       (reduce
        (fn [current-coordinates next]
-         (let [command (commands (next :command))]
-           (command current-coordinates (next :amount))))
-       {:horizontal-position 0 :depth 0}
+         (let [command (commands (next :command))
+               result (command current-coordinates (next :amount))]
+           (do
+             result)))
+       {:horizontal-position 0 :depth 0 :aim 0}
        parsed-lines)]
 
   (*
