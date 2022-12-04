7647c751f437a9f1eef41ea49cb11bb5028738a1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-12-04 19:22:29 +0000

    Add solution for day 4, part 2

--- 2022/day4.clj
+++ 2022/day4.clj
@@ -28,9 +28,30 @@
 (defn fully-contained-ranges [section-assignments]
   (filter fully-contained? section-assignments))
 
-(->
- "day_4_input"
- (read-lines)
- (parse-section-assignments-per-pair)
- (fully-contained-ranges)
- (count))
+(defn overlap? [[first-range second-range]]
+  (or
+   (and
+    (<= (start first-range) (start second-range))
+    (<= (start second-range) (end first-range)))
+
+   (and
+    (<= (start second-range) (start first-range))
+    (<= (start first-range) (end second-range)))))
+
+(defn overlapping-ranges [ranges]
+  (filter overlap? ranges))
+
+(let [ranges
+      (->
+       "day_4_input"
+       (read-lines)
+       (parse-section-assignments-per-pair))]
+  (->
+   ranges
+   (fully-contained-ranges)
+   (count))
+
+  (->
+   ranges
+   (overlapping-ranges)
+   (count)))
