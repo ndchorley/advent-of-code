2e2200b291cb332686881c8644a57f4df1d8ed4e
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-12-02 19:22:57 +0000

    Remove now unused Solutions.for_day

--- 2023/advent_of_code/bin/solutions.ml
+++ 2023/advent_of_code/bin/solutions.ml
@@ -1,11 +1,5 @@
 open Puzzle
 
-let for_day day = 
-  match day with
-  | 1 -> Day1.solution
-  | _ -> fun () -> "Not implemented yet"
-
-
 let for_puzzle puzzle =
   match puzzle with
   | {day = 1; part = 1} -> Day1.solution
