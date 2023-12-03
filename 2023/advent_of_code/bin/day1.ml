c77cef81ea25819ee2fd692412a1c17c1c61ed7c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-12-03 06:45:02 +0000

    Refactor to allow passing in of function to calculate calibration value for a line

--- 2023/advent_of_code/bin/day1.ml
+++ 2023/advent_of_code/bin/day1.ml
@@ -3,19 +3,20 @@ let first_and_last digits =
   let last = Lists.last digits in
     [first; last]
 
-let calibration_value line =
+let calibration_value_part_1 line =
   Strings.to_char_list line
   |> List.filter Characters.is_digit
   |> first_and_last
   |> Strings.from_char_list
   |> int_of_string
   
-let calibration_values calibration_document =
+let calibration_values calibration_value calibration_document =
   List.map calibration_value calibration_document
 
 let part_1_solution () =
   let calibration_document = Input.read_input "input/day1_input" in
     calibration_document
-    |> calibration_values
+    |> calibration_values calibration_value_part_1
     |> Integers.sum
     |> string_of_int
+   
