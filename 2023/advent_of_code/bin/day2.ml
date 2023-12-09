227879f447f469ef881c5f5204079f9ea671acad
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-12-09 11:00:09 +0000

    Add solution for day 2, part 1

---
+++ 2023/advent_of_code/bin/day2.ml
@@ -0,0 +1,62 @@
+type subset = {red: int; green: int; blue: int}
+
+type game = {id: int; subsets: subset list}
+
+let parse_id id_part =
+  id_part
+  |> Str.split (Str.regexp " ")
+  |> Lists.last
+  |> int_of_string
+
+let count_of colour subset_string =
+  let regex = Str.regexp ("\\([0-9]+\\) " ^ colour) in
+    try
+      let _ = Str.search_forward regex subset_string 0 in
+        Str.matched_group 1 subset_string |> int_of_string
+    with
+      Not_found -> 0
+
+let parse_subset subset_string =
+  {
+    red=(count_of "red" subset_string);
+    blue=(count_of "blue" subset_string);
+    green=(count_of "green" subset_string)
+  }
+
+
+let parse_subsets subsets_part =
+  subsets_part
+  |> Str.split (Str.regexp ";")
+  |> List.map parse_subset
+
+let parse_game line =
+  let parts = Str.split (Str.regexp ":") line in
+    {
+      id=parse_id (List.hd parts);
+      subsets=parse_subsets (Lists.last parts)
+    }
+
+let parse_games lines = List.map parse_game lines
+
+let record_of_games =
+  Input.read_input "input/day2_input"
+  |> parse_games
+
+let is_possible game =
+  List.for_all
+    (fun subset ->
+      subset.red <= 12 && subset.green <= 13 && subset.blue <= 14)
+    game.subsets
+
+let only_possible_games games =
+  List.filter is_possible games
+
+let only_the_ids games =
+  List.map (fun game -> game.id) games
+
+let part_1_solution () =
+  record_of_games
+  |> only_possible_games
+  |> only_the_ids
+  |> Integers.sum
+  |> string_of_int
