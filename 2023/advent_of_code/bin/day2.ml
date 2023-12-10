ef6f19dc8277c919079289bc5422f519a4f21379
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-12-10 09:44:03 +0000

    Add solution for day 2, part 2

--- 2023/advent_of_code/bin/day2.ml
+++ 2023/advent_of_code/bin/day2.ml
@@ -60,3 +60,30 @@ let part_1_solution () =
   |> only_the_ids
   |> Integers.sum
   |> string_of_int
+
+let fewest_cubes game =
+  List.fold_left
+    (fun result_so_far subset ->
+      {
+        red=(Int.max subset.red result_so_far.red);
+        green=(Int.max subset.green result_so_far.green);
+        blue=(Int.max subset.blue result_so_far.blue)
+      }
+    )
+    {red=0; green=0; blue=0}
+    game.subsets
+
+let find_fewest_cubes_per_game games =
+  List.map fewest_cubes games
+
+let powers subsets =
+  List.map
+    (fun subset -> subset.red * subset.green * subset.blue)
+    subsets
+
+let part_2_solution () =
+    record_of_games
+    |> find_fewest_cubes_per_game
+    |> powers
+    |> Integers.sum
+    |> string_of_int
--- 2023/advent_of_code/bin/solutions.ml
+++ 2023/advent_of_code/bin/solutions.ml
@@ -5,4 +5,5 @@ let for_puzzle puzzle =
   | {day = 1; part = 1} -> Day1.part_1_solution
   | {day = 1; part = 2} -> Day1.part_2_solution
   | {day = 2; part = 1} -> Day2.part_1_solution
+  | {day = 2; part = 2} -> Day2.part_2_solution
   | _ -> fun () -> "Not implemented yet"
