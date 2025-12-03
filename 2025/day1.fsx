44dd938c1049281e968b9f3d6933540526870801
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-12-03 17:23:25 +0000

    Introduce parseRotation to return a positive/negative value depending on whether the rotation is right/left

--- 2025/day1.fsx
+++ 2025/day1.fsx
@@ -1,2 +1,14 @@
+let parseRotation (line: string) =
+    let direction = line.Chars 0
+    let amount =
+        line.Substring 1
+        |> System.Convert.ToInt32
+
+    if direction.Equals 'L' then
+        amount * -1
+    else
+        amount
+ 
 System.IO.File.ReadLines "day1-input"
-|> Seq.iter (fun line -> printfn "%s" line) 
+|> Seq.map parseRotation
+|> Seq.iter (fun rotation -> printfn "%d" rotation) 
