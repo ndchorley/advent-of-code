f39dff66ddbdbaa602e18364cfbb47feace6784a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-12-05 07:43:16 +0000

    Rename amount to distance, since that's what the puzzle uses

--- 2025/day1.fsx
+++ 2025/day1.fsx
@@ -1,14 +1,14 @@
 let parseRotation (line: string) =
     let direction = line.Chars 0
-    let amount =
+    let distance =
         line.Substring 1
         |> System.Convert.ToInt32
 
     if direction.Equals 'L' then
-        amount * -1
+        distance * -1
     else
-        amount
- 
+        distance
+
 System.IO.File.ReadLines "day1-input"
 |> Seq.map parseRotation
 |> Seq.iter (fun rotation -> printfn "%d" rotation) 
