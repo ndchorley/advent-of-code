9b19eaef840ecf3c3c9c4716b3256731493ebaf4
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-12-07 11:49:08 +0000

    Add solution for day 2, part 1

---
+++ 2025/day2.fsx
@@ -0,0 +1,54 @@
+type Range = { first: uint64; last: uint64}
+
+let rangeFrom (string: string) : Range =
+    let parts = string.Split "-"
+    
+    {
+        first = LanguagePrimitives.ParseUInt64 parts[0]
+        last = LanguagePrimitives.ParseUInt64 parts[1]
+    }
+
+let parseRanges (line: string) =
+    line.Split ","
+    |> Array.map rangeFrom
+
+
+let isEven value = value % 2 = 0
+
+let isInvalid (id: string) =
+    let halfWay = id.Length / 2
+
+    let firstHalf = id.Substring(0, halfWay)
+    let secondHalf = id.Substring(halfWay)
+
+    firstHalf = secondHalf
+
+let findInvalidIds range =
+    seq { range.first .. range.last }
+    |> Seq.map (fun id -> id.ToString())
+    |> Seq.filter (fun id -> isEven id.Length)
+    |> Seq.filter isInvalid
+    |> Seq.map (fun id -> LanguagePrimitives.ParseUInt64 id)
+    |> List.ofSeq
+
+let invalidIdsInEach ranges =
+    Array.map findInvalidIds ranges
+
+let asOneList invalidIdsPerRange =
+    Array.fold
+        (fun allIds idsForARange -> List.append allIds idsForARange)
+        []
+        invalidIdsPerRange
+
+let addThemUp ids =
+    List.fold
+        (fun total id -> total + id)
+        0UL
+        ids
+
+System.IO.File.ReadAllText "day2-input"
+|> parseRanges
+|> invalidIdsInEach
+|> asOneList
+|> addThemUp
+|> (fun total -> printfn $"{total}")
